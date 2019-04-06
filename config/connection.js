0868070dddfa98bde662b059342c86d7b2a8b89d
atiftariq786 <atiftariq786@hotmail.com>
2019-04-06 00:38:06 -0700

    Add styling and setup UI

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "asialink7861",
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: "asialink7861",
+        database: "burgers_db"
+    })
+};
+
+
+
 
 // Make connection.
 connection.connect(function(err) {
